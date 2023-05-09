52c1d74b75118705113f30ec3696d71fce18bcff
baolong281 <baolong281@gmail.com>
2023-05-09 01:34:40 -0500

    matching sort of works

--- src/extractor.rs
+++ src/extractor.rs
@@ -37,9 +37,10 @@ impl Extractor {
 	}
 
 	pub fn extract(&mut self, frame: Mat) -> opencv::Result<(Mat, Vector<KeyPoint>, Vec<(KeyPoint, KeyPoint)>)> {
+
+		//convert to grayscale and get features
 		let mut gray = Mat::default();
 		let mut feats = Mat::default();
-
 		imgproc::cvt_color(&frame, &mut gray, imgproc::COLOR_BGR2GRAY, 1)?;
 		imgproc::good_features_to_track(
 			&gray,
@@ -52,6 +53,7 @@ impl Extractor {
 			false,
 			0.04)?;
 
+		//turn features mat into vector of keypoints
 		let mut kps = feats
 			.iter::<Point2f>()?
 			.map(|(_, point)| {
@@ -59,36 +61,43 @@ impl Extractor {
 			})
 			.collect::<Vector<KeyPoint>>();
 
+
+		//computing orbs
 		let mut orb_desc = Mat::default();
 		let mask = Mat::default();
 		self.orb.compute(&gray, &mut kps, &mut orb_desc)?;
 
-		let mut knn_vec = Vector::default();
-		let mut knn: Vec<(KeyPoint, KeyPoint)> = vec![];
+		let mut matches_vec= Vector::default();
+		let mut matches: Vec<(KeyPoint, KeyPoint)> = vec![];
 
+		//run the matche if last is some
 		if let Some(last) = &mut self.last {
-			self.matcher.knn_train_match(&last.desc, &orb_desc, &mut knn_vec, 2, &mask, false)?;
+			self.matcher.knn_train_match(&last.desc, &orb_desc, &mut matches_vec, 2, &mask, false)?;
 			last.img = frame.clone();
 			last.kps = kps.clone();
 			last.desc = orb_desc.clone();
 
-			knn = knn_vec
+			//convert vector of matches to tuples of both keypoints
+			matches = matches_vec 
 				.iter()
 				.filter(|m1| {
 					m1.get(0).unwrap().distance < m1.get(1).unwrap().distance * 0.75 
 				})
+				.filter(|m1| {
+					m1.get(0).unwrap().train_idx < kps.len() as i32 && m1.get(0).unwrap().query_idx < kps.len() as i32
+				})
 				.map(|x| {
-					println!("{:?}", x.get(0).unwrap().query_idx as usize);
 					(kps.get(x.get(0).unwrap().query_idx as usize).unwrap(),
 					last.kps.get(x.get(0).unwrap().train_idx as usize).unwrap()
 				)
 				})
 				.collect();
 		} else {
+			//declare last if last is none
 			self.last = Some(LastData {img: frame.clone(), kps: kps.clone(), desc: orb_desc.clone()});
 		}
 
-		Ok((frame, kps , knn))
+		Ok((frame, kps, matches))
 	}
 
 	pub fn get_last(&self) -> Option<(Mat, Vector<KeyPoint>, Mat)> {
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod extractor;
 use extractor::Extractor;
 use opencv::core::*;
+use opencv::imgproc;
+use opencv::imgproc::LINE_8;
 use std::env;
 use std::process;
 use std::path::Path;
@@ -11,19 +13,20 @@ use opencv::{videoio::{
 	CAP_ANY}, };
 
 fn main() {
+	//read path 
 	let path = env::args()
 		.nth(1).unwrap_or_else(|| {
 			println!("Error: Missing required argument file path");
 			process::exit(1);
 		});
 
+	//check if file exists
 	check_file(&path).unwrap_or_else(|_| {
 			println!("Error: File invalid or does not exist");
 			process::exit(1);
 	});
 
 	let mut capture = VideoCapture::from_file(&path, CAP_ANY ).unwrap();
-
 	let mut frame = Mat::default();
 	let mut ext = Extractor::new().unwrap_or_else(|e| {
 		println!("{}", e);
@@ -55,17 +58,30 @@ fn process_frame(frame: &mut Mat, ext: &mut Extractor) -> opencv::Result<()> {
 			return Ok(());
 		}
 	};
-
 	let (img_2, kps_2, matches) = ext.extract(frame.clone())?;
 
-	let keypoints = matches
-		.into_iter()
-		.map(|(x, y)| {
-			x
+	let matches: Vec<(Point, Point)> = matches.into_iter()
+		.map(|(u, v)| {
+			let u = u.pt();
+			let v = v.pt();
+			print!("{:?}   ", u);
+			println!("{:?}", v);
+			(Point::new(u.x as i32, u.y as i32), Point::new(v.x as i32, v.y as i32))
 		})
 		.collect();
 
-	features2d::draw_keypoints(&frame.clone(), &keypoints, frame, Scalar::all(-1.0), features2d::DrawMatchesFlags::DEFAULT)?;
+	matches.into_iter()
+		.for_each(|(u, v)| {
+			imgproc::line(
+				frame,
+				u,
+				v,
+				Scalar::new(0.0, 255.0, 0.0, 0.0),
+				1,
+				imgproc::LINE_8,
+				0
+			).unwrap()
+		});
 
 	opencv::highgui::imshow("rustslam", frame)?;
 	opencv::highgui::wait_key(400)?;
